876f83a3c6e784e04f4bd1f024d4d198a945eb21
testone <testone@DESKTOP-QFVGV7R>
2026-06-07 16:59:48 +0300

    latest update and deploy3 version

--- run_cmd.bat
+++ run_cmd.bat
@@ -1,6 +1,6 @@
 @echo off
 
-cd /d C:\Users\alexeyt\source\repos\LscriptWin
-cmd.exe /K
+cd /d C:\Users\testone\source\repos\LscriptWin
+cmd.exe /K "python .\lsbb_deploy3.py"
 
-REM start "DEPLOYMENT" cmd.exe /K "cd /d C:\Users\alexeyt\source\repos\LscriptWin"
+
